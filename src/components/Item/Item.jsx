6eefa66ca0ad24f13e3b32273a28dbcd98b460e6
manuel <plnmn25@gmail.com>
2021-11-23 00:59:20 -0300

    no aparecen las imagenes - falta el delay

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,29 +1,31 @@
+
+
 export const Products = [
     {
     name: 'Petway Chico',
     price: '$1000',
-    img:'',
+    img:'petwayc',
     stock:'4',
     id: '1',
     },
     {
     name: 'Petway Mediano',
     price: '$1200',
-    img:'',
+    img:'petwaym',
     stock:'2',
     id: '2',
     },
     {
     name: 'Rascador',
     price: '$650',
-    img:'',
+    img:'rascador',
     stock:'14',
     id: '3',
     },
     {
     name: 'Mordisco',
     price: '$550',        
-    img:'',
+    img:'mordisco',
     stock:'5',
     id: '4',   
     }
--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -2,6 +2,10 @@ import {Products} from '../Item/Item.jsx'
 import {Fragment} from 'react/cjs/react.production.min'
 import ItemCount from '../ItemCount/ItemCount.jsx'
 import './ItemList.css'
+import mordisco from './mordisco.png'
+import petwayc from './petwayc.png'
+import petwaym from './petwaym.png'
+import rascador from './rascador.png'
 
 function ItemList(){
     return(
@@ -9,10 +13,10 @@ function ItemList(){
         {
             Products.map((product,index) => {
                 return(
-                    <div key={index}>
+                    <div className="Container" key={index}>
                         <h1 className="Product">{product.name}</h1>
-                        <img src={product.img} alt='' />
-                        <p className="Price">{product.price}</p>
+                        <img src={product.img} alt='Product image' />
+                        <h1 className="Price">{product.price}</h1>
                         <ItemCount productstock={product.stock} />
                     </div>
                 )
